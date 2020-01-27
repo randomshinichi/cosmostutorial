3afeb3dfb74185f746ba9b10306a28a1081db1ca
Andrew Chiw <randomshinichi4869@gmail.com>
2020-01-28 00:46:01 +0100

    everything setup, no copypasting yet

---
+++ x/nameservice/genesis.go
@@ -0,0 +1,25 @@
+package nameservice
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/randomshinichi/cosmostutorial/x/nameservice/internal/types"
+)
+
+// InitGenesis initialize default parameters
+// and the keeper's address to pubkey map
+func InitGenesis(ctx sdk.Context, k Keeper, /* TODO: Define what keepers the module needs */, data types.GenesisState) {
+
+	// TODO: Define logic for when you would like to initalize a new genesis
+
+	return []abci.ValidatorUpdate{}
+}
+
+// ExportGenesis writes the current store values
+// to a genesis file, which can be imported again
+// with InitGenesis
+func ExportGenesis(ctx sdk.Context, k Keeper) (data types.GenesisState) {
+
+	// TODO: Define logic for exporting state
+	return types.NewGenesisState()
+}
